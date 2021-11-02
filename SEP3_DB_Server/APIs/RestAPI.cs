5d8b3be45ae3cc9d5f87e6d0eb816f61d56f8195
Young <andreasyoungandersen@gmail.com>
2021-11-02 17:53:39 +0100

    Controller file init

---
+++ SEP3_DB_Server/APIs/RestAPI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SEP3_DB_Server.APIs
+{
+	public class RestAPI
+	{
+		private List<ControllerBase> _controllers;
+
+		public RestAPI()
+		{
+
+			_controllers = new();
+		}
+	}
+}
---
+++ SEP3_DB_Server/APIs/RestControllers/SnorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SEP3_DB_Server.APIs.Controllers
+{
+	[ApiController][Route("[controller]")]
+	public class SnorController : ControllerBase
+	{
+		
+	}
+}
