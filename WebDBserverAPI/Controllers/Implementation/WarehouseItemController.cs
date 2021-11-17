c3d9f6cfa3636f49c462e3fb9d98711903e65ef9
Sebastian <280301@via.dk>
2021-11-17 17:09:57 +0100

    WareHouseItemController - Næsten færdig, mangler kun Post

--- WebDBserverAPI/Controllers/Implementation/WarehouseItemController.cs
+++ WebDBserverAPI/Controllers/Implementation/WarehouseItemController.cs
@@ -10,34 +10,53 @@ namespace WebDBserverAPI.Controllers
     [ApiController] [Route("[controller]")]
     public class WarehouseItemController : ControllerBase, WarehouseItemControllerI
     {
-        private DbContext _dbContext;
+        private DbContext _database;
 
         public WarehouseItemController(DbContext dbContext)
         {
             Console.WriteLine("WarehouseItemController instantiated");
-            _dbContext = dbContext;
+            _database = dbContext;
         }
 
-
-        public Task<ActionResult> GetWarehouseItemAsync(int warehouseItemId)
+        [HttpGet]
+        [Route("{warehouseItemId}")]
+        public async Task<ActionResult> GetWarehouseItemAsync([FromRoute] int warehouseItemId)
         {
-            //TODO: Lav mig
-            throw new NotImplementedException();
+            WarehouseItem returnValue = await _database.FindAsync<WarehouseItem>(warehouseItemId);
+            if (returnValue != null)
+            {
+                Console.WriteLine($"Sending value, {returnValue?.Id}, to Requesting Client");
+                return Ok(returnValue);
+            }
+            else
+            {
+                return NotFound("null");
+            }
         }
 
-        public Task<ActionResult> PutWarehouseItemAsync(WarehouseItem warehouseItem)
+        [HttpPut]
+        public async Task<ActionResult> PutWarehouseItemAsync([FromBody] WarehouseItem warehouseItem)
         {
-            //TODO: Lav mig
-            throw new NotImplementedException();
+            Console.WriteLine("Successfully entered WarehouseItemController.PutWarehouseItemAsync()");
+            await _database.AddAsync(warehouseItem);
+            await _database.SaveChangesAsync();
+            return Created($"/WarehouseItem/{warehouseItem.Id}", warehouseItem);
         }
 
-        public Task<ActionResult> DeleteWarehouseItemAsync(int warehouseItemId)
+        [HttpDelete]
+        [Route("{warehouseItemId}")]
+        public async Task<ActionResult> DeleteWarehouseItemAsync([FromRoute] int warehouseItemId)
         {
-            //TODO: Lav mig
-            throw new NotImplementedException();
+            Console.WriteLine($"Attempting to delete warehouseItem ({warehouseItemId})");
+            WarehouseItem warehouseItemToDelete = await _database.FindAsync<WarehouseItem>(warehouseItemId);
+            _database.Remove(warehouseItemToDelete);
+            await _database.SaveChangesAsync();
+            return Ok(warehouseItemToDelete);
         }
 
-        public Task<ActionResult> PostWarehouseItemAsync(int warehouseItemId, WarehouseItem warehouseItem)
+        [HttpPost]
+        [Route("{warehouseItemId}")]
+        public async Task<ActionResult> PostWarehouseItemAsync([FromRoute] int warehouseItemId, [FromBody] WarehouseItem warehouseItem)
         {
             //TODO: Lav mig
             throw new NotImplementedException();
