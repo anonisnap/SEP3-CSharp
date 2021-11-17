491a7e7600011dbe32dba344467bcb65646cfd33
Sebastian <280301@via.dk>
2021-11-17 10:10:42 +0100

    Centralizeret entities til eget projekt og fjernet fra client og DB server samt lavet dependencies på det nye projekt

--- WebDBserverAPI/Controllers/SpikeController.cs
+++ WebDBserverAPI/Controllers/SpikeController.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using WebDBserverAPI.Models;
+using SEP3_WebServerClient.Models;
 
 namespace WebDBserverAPI.Controllers
 {
--- WebDBserverAPI/DataAccess/SEP_DBContext.cs
+++ WebDBserverAPI/DataAccess/SEP_DBContext.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using WebDBserverAPI.Models;
+using SEP3_WebServerClient.Models;
 
 namespace WebDBserverAPI.DataAccess
 {
@@ -12,6 +12,10 @@ namespace WebDBserverAPI.DataAccess
             //Name of database
             optionsBuilder.UseNpgsql(ConnStr.Get(), options => options.UseAdminDatabase("geoxbaal"));
             //optionsBuilder.UseSqlite(@"Data Source = D:\GitHubProjects\SEP 3\SEP3_DB_Server\WebDBserverAPI\warehouse.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Spike>().HasKey(spike => spike.SpikeName);
         }
     }
 }
