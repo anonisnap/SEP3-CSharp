9e0e32f2c87abe04e472479f67667536bbbf93d0
Young <andreasyoungandersen@gmail.com>
2021-11-17 15:00:47 +0100

    Basic Model Items added

---
+++ Entities/Models/WarehouseItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class WarehouseItem
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+    }
+}
