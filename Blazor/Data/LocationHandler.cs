e6b86d6f16ced264c8894924720ebec4dcb50957
Frederik <frederik.m.hansen@gmail.com>
2021-11-29 11:07:15 +0100

    Page næsten done til M-1: Manager; Create Location

---
+++ Blazor/Data/LocationHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Entities.Models;
+
+namespace Blazor.Data
+{
+    public class LocationHandler : ILocationHandler
+    {
+        public Task CreateLocation(Location location)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
