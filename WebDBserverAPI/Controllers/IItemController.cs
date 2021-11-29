4d04a00c52db88d29da0cc8899d3f6d725967ef1
Sebastian <280301@via.dk>
2021-11-29 09:40:04 +0100

    Renaming

---
+++ WebDBserverAPI/Controllers/IItemController.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebDBserverAPI.Controllers
+{
+    public interface IItemController
+    {
+        //TODO: Jeg mangler i astah ;(
+        Task<ActionResult> GetItemAsync(int itemId);
+        Task<ActionResult> PutItemAsync(Item item);
+        Task<ActionResult> DeleteItemAsync(int itemId);
+        Task<ActionResult> PostItemAsync(int itemId, Item item);
+        
+    }
+}
