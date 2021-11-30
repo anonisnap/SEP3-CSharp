a4e5d81fa1bfadb46db36f9b65b069360f2557af
Young <andreasyoungandersen@gmail.com>
2021-11-30 10:29:37 +0100

    Repo Injection from Startup

--- WebDBserverAPI/Startup.cs
+++ WebDBserverAPI/Startup.cs
@@ -1,20 +1,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using DataBaseAccess;
+using DataBaseAccess.DataRepos;
+using DataBaseAccess.DataRepos.Impl;
+using Entities.Models;
 
-namespace WebDBserverAPI {
+namespace WebDBserverAPI
+{
 	public class Startup {
 		public Startup(IConfiguration configuration) {
 			Configuration = configuration;
@@ -32,6 +29,8 @@ namespace WebDBserverAPI {
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebDBserverAPI", Version = "v1" });
 			});
 			services.AddSingleton<DbContext, SEP_DBContext>();
+			services.AddScoped<IDataRepo<Item>, ItemDataRepo>();
+			services.AddScoped<IDataRepo<Location>, LocationDataRepo>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
