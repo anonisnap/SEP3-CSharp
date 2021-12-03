4111f9ac63c8dadf3b3beefaf0f03eeaded527a9
Sebastian <280301@via.dk>
2021-12-03 11:44:04 +0100

    Forgot the Migrations - Here they are

---
+++ DataBaseAccess/Migrations/20211203103957_4_updatedItemLocationDb.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace DataBaseAccess.Migrations
+{
+    public partial class _4_updatedItemLocationDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ItemLocationsDb",
+                table: "ItemLocationsDb");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "ItemLocationsDb",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ItemLocationsDb",
+                table: "ItemLocationsDb",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemLocationsDb_ItemId_LocationId",
+                table: "ItemLocationsDb",
+                columns: new[] { "ItemId", "LocationId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ItemLocationsDb",
+                table: "ItemLocationsDb");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ItemLocationsDb_ItemId_LocationId",
+                table: "ItemLocationsDb");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "ItemLocationsDb");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ItemLocationsDb",
+                table: "ItemLocationsDb",
+                columns: new[] { "ItemId", "LocationId" });
+        }
+    }
+}
---
+++ DataBaseAccess/Migrations/TestDb/20211203104139_4_updatedItemLocationDb.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataBaseAccess.Migrations.TestDb
+{
+    public partial class _4_updatedItemLocationDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemLocationsDB_Items_ItemId",
+                table: "ItemLocationsDB");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemLocationsDB_Locations_LocationId",
+                table: "ItemLocationsDB");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ItemLocationsDB",
+                table: "ItemLocationsDB");
+
+            migrationBuilder.RenameTable(
+                name: "ItemLocationsDB",
+                newName: "ItemLocationsDb");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ItemLocationsDB_LocationId",
+                table: "ItemLocationsDb",
+                newName: "IX_ItemLocationsDb_LocationId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "ItemLocationsDb",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ItemLocationsDb",
+                table: "ItemLocationsDb",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemLocationsDb_ItemId_LocationId",
+                table: "ItemLocationsDb",
+                columns: new[] { "ItemId", "LocationId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemLocationsDb_Items_ItemId",
+                table: "ItemLocationsDb",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemLocationsDb_Locations_LocationId",
+                table: "ItemLocationsDb",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemLocationsDb_Items_ItemId",
+                table: "ItemLocationsDb");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemLocationsDb_Locations_LocationId",
+                table: "ItemLocationsDb");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ItemLocationsDb",
+                table: "ItemLocationsDb");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ItemLocationsDb_ItemId_LocationId",
+                table: "ItemLocationsDb");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "ItemLocationsDb");
+
+            migrationBuilder.RenameTable(
+                name: "ItemLocationsDb",
+                newName: "ItemLocationsDB");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ItemLocationsDb_LocationId",
+                table: "ItemLocationsDB",
+                newName: "IX_ItemLocationsDB_LocationId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ItemLocationsDB",
+                table: "ItemLocationsDB",
+                columns: new[] { "ItemId", "LocationId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemLocationsDB_Items_ItemId",
+                table: "ItemLocationsDB",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemLocationsDB_Locations_LocationId",
+                table: "ItemLocationsDB",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
