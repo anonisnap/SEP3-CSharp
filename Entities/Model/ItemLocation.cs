2523035b79f6eec83add61eb2ac5c3c062e1dedc
Young <andreasyoungandersen@gmail.com>
2021-12-05 19:02:06 +0100

    Added Equals Methods

--- Entities/Model/ItemLocation.cs
+++ Entities/Model/ItemLocation.cs
@@ -1,19 +1,20 @@
-﻿namespace Entities.Models
-{
-    public class ItemLocation
-    {
-        public int Id { get; set; }
-        public Location Location { get; set; }
-        public Item Item { get; set; }
-        
-        public int Amount { get; set; }
+﻿namespace Entities.Models {
+	public class ItemLocation {
+		public int Id { get; set; }
+		public Location Location { get; set; }
+		public Item Item { get; set; }
 
-        public override string ToString()
-        {
-            return $"LocationID: {Id}" +
-                   $"\nLocation: {Location} " +
-                   $"\nItem: {Item} " +
-                   $"\nAmount: {Amount}";
-        }
-    }
+		public int Amount { get; set; }
+
+		public override string ToString( ) {
+			return $"LocationID: {Id}" +
+				   $"\nLocation: {Location} " +
+				   $"\nItem: {Item} " +
+				   $"\nAmount: {Amount}";
+		}
+		public override bool Equals(object obj) {
+			return (obj?.GetType( ) == typeof(ItemLocation)) && Id == ((ItemLocation) obj).Id && Amount == ((ItemLocation) obj).Amount && Item.Equals(((ItemLocation) obj).Item) && Location.Equals(((ItemLocation) obj).Location);
+		}
+
+	}
 }
--- Entities/Model/Location.cs
+++ Entities/Model/Location.cs
@@ -1,17 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Entities.Models
-{
-    public class Location
-    {
-        public int Id { get; set;}
-        [Required, MaxLength(256)]
-        public string Description { get; set; }
+namespace Entities.Models {
+	public class Location {
+		public int Id { get; set; }
+		[Required, MaxLength(256)]
+		public string Description { get; set; }
 
-        public override string ToString()
-        {
-            return $"Id: {Id} " +
-                   $"\n Description: {Description}";
-        }
-    }
+		public override string ToString( ) {
+			return $"Id: {Id} " +
+				   $"\n Description: {Description}";
+		}
+
+		public override bool Equals(object obj) {
+			return (obj?.GetType( ) == typeof(Location)) && Id == ((Location) obj).Id && Description == ((Location) obj).Description;
+		}
+	}
 }
