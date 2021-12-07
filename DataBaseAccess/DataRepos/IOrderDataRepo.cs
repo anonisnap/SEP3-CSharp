603f10682afba39c6595723d328785957a8866b5
Sebastian <280301@via.dk>
2021-12-07 17:04:24 +0100

    OrderDataRepo created, missing method implementations

---
+++ DataBaseAccess/DataRepos/IOrderDataRepo.cs
@@ -0,0 +1,9 @@
+﻿using Entities.Models;
+
+namespace DataBaseAccess.DataRepos
+{
+    public interface IOrderDataRepo : IDataRepo<Order>
+    {
+        
+    }
+}
---
+++ DataBaseAccess/DataRepos/Impl/OrderDataRepo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseAccess.DataRepos.Impl
+{
+    public class OrderDataRepo : IOrderDataRepo
+    {
+        private WarehouseDbContext _warehouseDbContext;
+
+        public OrderDataRepo(WarehouseDbContext dbContext)
+        {
+            _warehouseDbContext = dbContext;
+        }
+        
+        public async Task<Order> AddAsync(Order obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Order> RemoveAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Order> UpdateAsync(Order itemLocation)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IList<Order>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Order> GetAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
