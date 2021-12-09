40f9ae1c0fb215ff9219ee8b9af0db98a1f690f2
Young <andreasyoungandersen@gmail.com>
2021-12-09 13:52:32 +0100

    Beginning of UserService

---
+++ GrpcClient/Clients/GrpcUserClient.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Models;
+using Grpc.Net.Client;
+using myGrpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T1Contracts.ServerCommunicationInterfaces;
+
+namespace GrpcClient.Clients {
+	public class GrpcUserClient : IUserDataServerComm {
+		private string _address;
+		private GrpcChannel _channel;
+		private UserService.UserServiceClient _client;
+
+		public GrpcUserClient(GRPCConnStr address) {
+			_address = address.GrpcAddress;
+		}
+		
+		public Task<User> LoginAsync(User user) {
+			throw new NotImplementedException( );
+		}
+
+		public Task Logout(User user) {
+			throw new NotImplementedException( );
+		}
+
+		private void Connect( ) {
+			_channel = GrpcChannel.ForAddress(_address);
+			_client = new UserService.UserServiceClient(_channel);
+		}
+
+		private async Task Disconnect( ) {
+			await _channel.ShutdownAsync( );
+			_client = null;
+		}
+	}
+}
---
+++ T1Contracts/ServerCommunicationInterfaces/IUserDataServerComm.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1Contracts.ServerCommunicationInterfaces {
+	public interface IUserDataServerComm {
+		Task<User> LoginAsync(User user);
+		Task Logout(User user);
+	}
+}
