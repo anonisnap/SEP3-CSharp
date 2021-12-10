1d14867b5a36cad3acd91b0be5055c6bb38ca57e
Frederik <frederik.m.hansen@gmail.com>
2021-12-10 10:30:50 +0100

    mere trash

--- Blazor/Pages/Show/Items.razor.cs
+++ Blazor/Pages/Show/Items.razor.cs
@@ -36,11 +36,6 @@ namespace Blazor.Pages.Show
                 });
         }
 
-        async Task DeleteItemLocation(ItemLocation itemLocation)
-        {
-            //move item to trashed list
-        }
-        
         void Open(string title, Type type, Dictionary<string, object> parameters, DialogOptions options)
         {
             Console.WriteLine("Dialog Opened");
