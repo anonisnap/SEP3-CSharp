a57ea98bde8a1f04af4aa03b0e83c04d08531248
Sebastian <280301@via.dk>
2021-12-11 13:17:40 +0100

    ToString for Order Entry

--- Entities/Model/OrderEntry.cs
+++ Entities/Model/OrderEntry.cs
@@ -6,5 +6,14 @@
         public int OrderId { get; set; }
         public int Amount { get; set; }
         public Item Item { get; set; }
+
+        public override string ToString()
+        {
+            return $"Order Entry\n " +
+                   $"Id: {Id}\n" +
+                   $"OrderId: {OrderId}\n" +
+                   $"Amount: {Amount}\n" +
+                   $"Item: {Item}";
+        }
     }
 }
